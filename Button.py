e124292864388676925f9b47b9c4322cf880d1e2
tavares1 <lucastavaresvasconcelos@gmail.com>
2019-08-08 14:26:16 -0300

    Interface dos botoes finalizada

---
+++ Button.py
@@ -0,0 +1,19 @@
+import pygame
+import utils as u
+
+class Button():
+    def __init__(self, text, color, text_color):
+        pygame.init()
+        self.text = text
+        self.color = color
+        self.text_color = text_color
+        self.font = pygame.font.SysFont("Candara",25)
+
+    def draw_button(self, screen, x, y, width, height):
+        pygame.draw.rect(screen, self.color , (x,y,width,height))
+        render_font = self.font.render(self.text, True, self.text_color)
+        text_size_w, text_size_h = render_font.get_size()
+        font_position_x = 250 - (text_size_w/2)
+        font_position_y = y + (text_size_h/2)
+        screen.blit( render_font, (font_position_x, font_position_y))
+        pygame.display.flip()
--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import pygame
 from Grid import Grid
+from Button import Button
+import utils as u
 
+# Inicializando o pygame, setando o tamanho da tela.
 pygame.display.init()
 window_size = (1200, 600)
 screen = pygame.display.set_mode(window_size)
-screen.fill((0, 0, 0))
+
+# Criando o grid( A estrela) e desenhando na tela.
+
+screen.fill((220, 220, 220))
 grid = Grid()
 grid.draw_grid(screen)
-pygame.display.flip()
+
+# Inicilizando e desenhando o botão de passar a vez.
+
+pass_turn_button = Button("Passar a vez", u.WHITE, u.BLUE)
+pass_turn_button.draw_button(screen,150,450,200,50)
+
+# Inicializando e desenhando o botão de desistir.
+
+give_up_button = Button("Desistir", u.WHITE, u.RED)
+give_up_button.draw_button(screen, 150, 525, 200, 50)
+
+# Recebendo toques e dinamica do jogo.
+
 done = False
 while not done:
     for event in pygame.event.get():
@@ -15,6 +33,9 @@ while not done:
             done = True
         if event.type == pygame.MOUSEBUTTONDOWN:
             position_mouse = pygame.mouse.get_pos()
+            print(position_mouse)
             x = position_mouse[0]
             y = position_mouse[1]
             grid.verify_position_in_matrix(x, y, screen)
+
+
